21bd2ea8ecea6bf7e17ae8834daf35aee5a9060e
Amerey <59344168+Amereyeu@users.noreply.github.com>
2025-04-04 20:44:06 +0200

    Update App.jsx

--- files/App.jsx
+++ files/App.jsx
@@ -1,30 +1,39 @@
 import { AnimatePresence } from "framer-motion";
+import { useLayoutEffect } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
-
 
 import Home from "./pages/Home";
 import NoPage from "./pages/NoPage";
 
-
 import Footer from "./components/layout/Footer";
-import ScrollTo from "./components/misc/ScrollTo";
+import ScrollToTop from "./components/misc/ScrollToTop";
 
 import "./App.scss";
 
 function App() {
-  const location = useLocation();
+  const Wrapper = ({ children }) => {
+    const location = useLocation();
+
+    useLayoutEffect(() => {
+      window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    }, [location.pathname]);
+
+    return children;
+  };
 
   return (
     <div className="App">
       <AnimatePresence mode="wait">
-        <Routes key={location.pathname} location={location}>
-          <Route path="*" element={<NoPage />} exact />
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <Wrapper>
+          <Routes>
+            <Route path="*" element={<NoPage />} exact />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Wrapper>
       </AnimatePresence>
 
       <Footer />
-      <ScrollTo />
+      <ScrollToTop />
     </div>
   );
 }
