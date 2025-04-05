779a8329c85d51a328ef4c73c4171477cac88160
Amerey <59344168+Amereyeu@users.noreply.github.com>
2025-04-05 08:29:43 +0200

    Update Footer.jsx

--- files/Footer.jsx
+++ files/Footer.jsx
@@ -1,12 +1,9 @@
 /*********************************************
  * footer section
  *********************************************/
-
 import { useState } from "react";
-import { HashLink } from "react-router-hash-link";
-
-import data from "../../data/nav";
-
+import { Link } from "react-router-dom";
+import data from "src/data/nav";
 
 function Footer() {
   const today = new Date();
@@ -16,60 +13,50 @@ function Footer() {
     <div className="footer-wrap">
       <footer className="footer">
         <div className="footer__info">
-          {/* <span></span> */}
-          <div className="footer__info__title">
+          <img src="../images/logo.jpg" alt="logo" />
 
-            <h3>Tornado Garage</h3>
-          </div>
-          <p>
-            Nostra sapien quis dictumst integer non vitae torquent. Posuere orci
-            enim quam cursus nam fusce convallis elementum consequat.
-          </p>
-
+          <p>slogan</p>
         </div>
 
         <div className="footer__links">
-          <h3>Links</h3>
+          <h3>Navigace</h3>
 
           <ul>
             {navigationData.map((navItem, i) => (
               <li key={i}>
-                <HashLink
-                  smooth
-                  to={navItem.to}
-                  className="footer__links__item">
+                <Link smooth to={navItem.to} className="footer__links__item">
                   {navItem.name}
-                </HashLink>
+                </Link>
               </li>
             ))}
           </ul>
         </div>
 
         <div className="footer__contact">
-          <h3>Address</h3>
-          <p>
-            Broadway 418 <br /> Nashville, TN 38040
-          </p>
+          <div className="address">
+            <h3>Adresa</h3>
+            <p>
+              address
+            </p>
+          </div>
 
-          <h3>Phone</h3>
-          <p>+420 123 456 789</p>
+          <div className="phone">
+            <h3>Telefon</h3>
+            <p>
+              <a href="tel:+420123">+420123</a>
+            </p>
+          </div>
 
-          <h3>Email</h3>
-          <p>info@tornadogarage.com</p>
-        </div>
-
-        <div className="footer__hours">
-          <h3>Opening hours</h3>
-
-          <ul>
-            <li>Mon - Fri: 9:00 - 17:00</li>
-            <li>Saturday: 09.00 - 12.00</li>
-            <li>Sunday: Closed</li>
-          </ul>
+          <div className="email">
+            <h3>Email</h3>
+            <p>
+              <a href="mailto:e.cz">e.cz</a>
+            </p>
+          </div>
         </div>
       </footer>
 
-      <p className="copy">&copy; 2008 - {today.getFullYear()} Tornado Garage</p>
+      <p className="copyright">&copy; {today.getFullYear()} 123</p>
     </div>
   );
 }
