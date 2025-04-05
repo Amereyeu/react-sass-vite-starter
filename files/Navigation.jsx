f1a2aca78e7bedafab31ba278ffa8f7247820a8e
Amerey <59344168+Amereyeu@users.noreply.github.com>
2025-04-05 08:24:54 +0200

    Update Navigation.jsx

--- files/Navigation.jsx
+++ files/Navigation.jsx
@@ -1,10 +1,10 @@
 /*********************************************
  * navigation
  *********************************************/
-
-import { useState, useEffect } from "react";
-import { HashLink } from "react-router-hash-link";
-import navigationItems from "../../data/nav";
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import navigationItems from "src/data/nav";
 
 function Navigation() {
   const [isNavOpen, setIsNavOpen] = useState(false);
@@ -12,46 +12,55 @@ function Navigation() {
   const [navigationData] = useState(navigationItems);
 
   const toggleNavigation = () => {
-    const body = document.getElementById("body");
     setIsNavOpen((prevState) => !prevState);
-    // body.classList.toggle("fixed_body");
   };
 
   useEffect(() => {
     const handleScroll = () => {
-      setIsSticky(window.scrollY > 23);
+      setIsSticky(window.scrollY > 100);
     };
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
-    <div className={`navigation-container ${isSticky ? "sticky2" : ""}`}>
+    <div className={`navigation-container ${isSticky ? "sticky" : ""}`}>
       <div className="navigation">
         <div className="row">
-          <div className="navigation__logo">Tornado Garage</div>
+          <div className="navigation__logo">
+            <Link smooth to="/" duration={50}>
+              <img src="/images/logo.jpg" alt="logo" />
+            </Link>
+          </div>
 
           <div
             className={`navigation__toggle ${isNavOpen ? "open" : ""}`}
-            onClick={toggleNavigation}>
+            onClick={toggleNavigation}
+          >
             <span></span>
             <span></span>
             <span></span>
           </div>
         </div>
 
-        <div className={`navigation__menu ${isNavOpen ? "collapse" : ""}`}>
-          {navigationData.map((navItem, i) => (
-            <HashLink
-              key={i}
-              smooth
-              to={navItem.to}
-              className="navigation__menu__item"
-              onClick={toggleNavigation}
-              duration={50}>
-              {navItem.name}
-            </HashLink>
-          ))}
+        <div className={`outer-menu ${isNavOpen ? "collapse-outer" : ""}`}>
+          <div
+            className={`navigation__menu ${isNavOpen ? "collapse-inner" : ""}`}
+          >
+            {navigationData.map((navItem, i) => (
+              <motion.div key={i}>
+                <Link
+                  smooth
+                  to={navItem.to}
+                  className="navigation__menu__item"
+                  onClick={toggleNavigation}
+                  duration={50}
+                >
+                  {navItem.name}
+                </Link>
+              </motion.div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
