18b56871a1e52c1525931cf8c7ca6dfce4010f68
Amerey <59344168+Amereyeu@users.noreply.github.com>
2025-04-05 13:00:35 +0200

    Update and rename nav.js to navigation.js

---
+++ files/navigation.js
@@ -0,0 +1,16 @@
+/*********************************************
+ * links in navigation
+ *********************************************/
+
+const navigationItems = [
+  {
+    to: "/",
+    name: "O nás",
+  },
+  {
+    to: "/kontakt",
+    name: "Contact",
+  },
+];
+
+export default navigationItems;
